187ad047b63b8fcf72d2b48e803554dd1ca75b92
Titu B G <mail4t2+github@gmail.com>
2024-10-17 04:37:20 +0000

    Added basic lists and styling

--- app/index.tsx
+++ app/index.tsx
@@ -1,36 +1,71 @@
 import {Button, StyleSheet, Text, TextInput, View} from "react-native";
+import { useState } from "react";
 
 function Index() {
-    return (
-        <View style={styles.appContainer}>
-            <View style={styles.inputContainer}>
-                <TextInput style={styles.textInput} placeholder='input your goal here'/>
-                <Button title='Add Goal'/>
-            </View>
-            <View>
-                <Text>List of goals...</Text>
-            </View>
-        </View>
-    );
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    setCourseGoals((c) => [...c, enteredGoalText]);
+  }
+
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="input your goal here" onChangeText={goalInputHandler} />
+        <Button title="Add Goal" onPress={addGoalHandler} />
+      </View>
+      <View style={styles.goalsContainer}>
+        {courseGoals.map((g) => (
+          <View key={g} style={styles.goalItem}>
+            <Text style={styles.goalText}>{g}</Text>
+          </View>
+        ))}
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    appContainer: {
-        paddingTop: 50,
-        paddingHorizontal: 16,
-    },
-    inputContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center'
-    },
-    textInput: {
-        borderWidth: 1,
-        borderColor: '#808080',
-        width: '70%',
-        marginRight: 8,
-        padding: 8,
-    }
-})
+  appContainer: {
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    flex: 1,
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#CCCCCC",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#808080",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 4,
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 5,
+    backgroundColor: "#5e0acc",
+  },
+  goalText: {
+    color: "white",
+    backgroundColor: "#5e0acc",
+  },
+});
+
 
 export default Index;
